b1e83e53890e856cc70f7c48f6dbaed52dfacffa
firdan <onpeci@gmail.com>
2024-06-06 14:23:47 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,38 @@
+Simple demo for WebSocket connection in NextJS, all WebSocket implementation is for client side only
+## Getting Started
+
+First, run the development server:
+
+```bash
+npm run dev
+# or
+yarn dev
+# or
+pnpm dev
+```
+
+Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+
+You can start editing the page by modifying `app/page.js`. The page auto-updates as you edit the file.
+
+This project uses [`next/font`](https://nextjs.org/docs/basic-features/font-optimization) to automatically optimize and load Inter, a custom Google Font.
+
+## Learn More
+
+To learn more about Next.js, take a look at the following resources:
+
+- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
+- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
+
+You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
+
+## Deploy on Vercel
+
+The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
+
+Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+
+## Attributions
+<a href="https://www.freepik.com/icon/chatbot_2068998#fromView=keyword&term=Robot&page=1&position=5">Chatbot Icon by Freepik</a>
+
+<a href="https://pixabay.com/id/vectors/akun-profil-avatar-pengguna-potret-6491185/">Avatar Icon Pixabay </a>
---
+++ src/app/page.js
@@ -0,0 +1,174 @@
+'use client'
+import Image from 'next/image'
+import styles from './page.module.css'
+import { createRef, useEffect, useState } from 'react'
+import io from 'socket.io-client'
+import Card from '@/components/card'
+import Actions from '@/components/actions'
+import {
+  CSSTransition,
+  TransitionGroup,
+} from 'react-transition-group';
+
+export default function Home() {
+  const [wsInstance, setWsInstance] = useState(null);
+  const [messages, setMessages] = useState([])
+  const [text, setText] = useState("")
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(null)
+  const [choices, setChoices] = useState([])
+  const [delay, setDelay] = useState(0)
+
+  useEffect(() => {
+    const objDiv = document.getElementById("messages")
+    if (objDiv.children.length > 1) {
+      objDiv.children[objDiv.children.length - 1].scrollIntoView({ behavior: 'smooth', block: 'end' })
+    }
+  }, [messages, loading])
+
+
+  function establishWebsocket() {
+    if (typeof window !== "undefined") {
+      const ws = new WebSocket("ws://localhost:8000/ws/chat/test/")
+      ws.onmessage = (e) => {
+        console.log("WS", e.data)
+        const parsed = JSON.parse(e.data)
+        switch (parsed.type) {
+          case "system":
+            if (parsed.message == "loading") {
+              setLoading(true)
+            } else if (parsed.message == "finished-loading") {
+              setLoading(false)
+            }
+            break;
+          case "action":
+            setChoices(parsed.choices)
+            break
+          default:
+            setMessages(arr => [...arr, parsed])
+
+        }
+      }
+      ws.onopen = (e) => {
+        console.log("Connected")
+        setLoading(false)
+      }
+      ws.onclose = (e) => {
+        console.log('Disconnected')
+      }
+      setWsInstance(ws)
+    }
+
+  }
+
+  useEffect(() => {
+    console.log('call ws')
+
+    setLoading(true)
+    establishWebsocket()
+
+    return () => {
+      // Cleanup on unmount if ws wasn't closed already
+      if (wsInstance?.readyState !== 3) {
+        console.log('call cleanup')
+        wsInstance.close()
+      }
+    }
+  }, [])
+
+  function submit() {
+    console.log(wsInstance)
+    console.log('attempt to send', wsInstance.readyState, text)
+    if (text !== "") {
+      wsInstance.send(JSON.stringify({ message: text, owner: "client" }))
+      setText("")
+
+    }
+  }
+
+  function submitCoinFlipAction(e) {
+    const answer = e.target.value
+    wsInstance.send(JSON.stringify({ message: "coinflip", answer: answer, owner: "client" }))
+    setChoices([])
+
+  }
+
+  console.log(wsInstance?.readyState)
+
+  return (
+    <main className={styles.main}>
+      <div className={styles.messages}>
+        <ul id="messages" className={styles.message_container}>
+          <TransitionGroup>
+            {
+              messages.map((val, index) => {
+                const message = val
+                let previousMessage;
+                if (index > 0) {
+                  previousMessage = messages[index - 1]
+                }
+                const drawAvatar = message.owner == "server" && ((index > 0 && previousMessage.owner !== "server") || index == 0)
+                return (
+                  <CSSTransition
+                    key={index}
+                    timeout={1000}
+                    classNames={ message.owner == "client" ? "" : "server-message-anim"}
+                  >
+                    <li key={index} className={styles.message}>
+                      <Card
+                        message={message.message}
+                        avatar={drawAvatar
+                          ? <Image src="/media/chatbot.png" width={60} height={60} />
+                          : null}
+                        userMessage={message.owner == "client"}
+                      />
+                    </li>
+                  </CSSTransition>
+                )
+              })
+            }
+            </TransitionGroup>
+            <Actions choices={choices} submitAction={submitCoinFlipAction} />
+            <CSSTransition
+              key={-1}
+              timeout={1000}
+              classNames=""
+              in={loading}
+            >
+              <>
+                {
+                  loading &&
+                  <li key={-1}>
+                    <Card
+                      message={<div className={styles.loader}></div>}
+                    />
+                  </li>
+                }
+              </>
+            </CSSTransition>
+            {
+              error && <li>
+                {error}
+              </li>
+            }
+
+        </ul>
+
+      </div>
+      <div className={styles.chat_input}>
+        <Image src="/media/user.png" width={60} height={60}></Image>
+        <textarea
+          value={text}
+          className={styles.input}
+          id="chat-input"
+          placeholder="Type here..."
+          onChange={(e) => setText(e.target.value)}
+          disabled={loading || choices.length > 0}
+        >
+        </textarea>
+        <button className={styles.send_button} onClick={submit}><Image src="/media/paper-plane.png" width={60} height={60}></Image></button>
+      </div>
+    </main>
+
+  )
+}
